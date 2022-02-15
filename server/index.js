d0df2c13b12472d75eabfa789fa5fc2777a83b88
araz saydu <arazsaydu@gmail.com>
2022-02-15 16:32:12 +0100

    connected backend to mongo db

--- server/index.js
+++ server/index.js
@@ -1,10 +1,32 @@
 
 
-require("dotenv").config();
 const express = require("express");
-const app = express();
+const coments = require("./data/coments");
+const server = express();
+//DB connection
+const connectDB = require("./config/db")
+require("dotenv").config();
+connectDB()
 
 const port = process.env.PORT ||5000
 
 
-app.listen(port, () => console.log(`server run on port${port}`));
+//first API`home APi`
+server.get("/", (req, res) =>{
+    res.send('home api')
+})
+
+server.get("/coments", (req, res) =>{
+    res.json(coments);
+})
+
+
+//test endpoint by searching for data with _id
+server.get("/coments/:id", (req, res) =>{
+    const coment = coments.find((item) => item._id === +req.params.id);
+
+    res.send(coment);
+    
+})
+
+server.listen(port, () => console.log(`server run on port ${port}`));
