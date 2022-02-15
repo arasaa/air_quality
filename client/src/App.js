bae367ca168a7ff88977910657a1cbc20a342c08
araz saydu <arazsaydu@gmail.com>
2022-02-15 22:29:52 +0100

    created routes for login and registration

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,8 @@ import Home from './components/screens/hom/Home';
 import Showing from './components/screens/showingData/Showing';
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Community from './components/screens/community/Community';
+import Login from './components/screens/login.js/Login';
+import Registration from './components/screens/registration/Registration';
 
 
 function App() {
@@ -16,6 +18,8 @@ function App() {
       <Route path="/" element={<Home />} exact />
       <Route path="/showing" element={<Showing />} />
       <Route path="/communty" element={<Community />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/registration" element={<Registration />} />
     </Routes>
     </main>
    <Footer />
