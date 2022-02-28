ed86e20ba521c7719c62943849021cf8c0765cb7
araz saydu <arazsaydu@gmail.com>
2022-02-28 15:43:50 +0100

    style

--- client/src/components/screens/registration/Registration.js
+++ client/src/components/screens/registration/Registration.js
@@ -11,23 +11,21 @@ const Registration = () => {
 <input type="text" name="Firstname" placeholder="Firstname" size="15" required />
 <label> Lastname: </label>
 <input type="text" name="lastname" placeholder="Lastname" size="15"required />
-<label for="psw"><b>email</b></label>
+<label htmlFor="psw"><b>email</b></label>
 <input type="text" placeholder="Enter Email" name="email" id="email" required />
-<label for="psw"><b>Password</b></label>
+<label htmlFor="psw"><b>Password</b></label>
 <input type="password" placeholder="Enter Password" name="psw" id="psw" required/>
 <label> Gender </label><br/>
-<input type="radio" value="Male" name="gender" checked /> Male
+<input type="radio" value="Male" name="gender" /> Male
 <input type="radio" value="Female" name="gender" /> Female
 <div/>
   <div>
-<form action="/action_page.php">
-  <label for="img">Select image:</label>
+
+  <label htmlFor="img">Select image:</label>
   <input type="file" id="img" name="img" accept="image/*"/>
-  <input type="submit"/>
-</form>
 </div>
-<label for="subject">Subject</label>
-<textarea id="subject" name="subject" placeholder="Write something.." style="height:200px"></textarea>
+<label htmlFor="subject">Subject</label>
+<textarea id="subject" name="subject" placeholder="Write something.." style={{height:"200px"}}></textarea>
 <input type="submit" value="Submit" />
 </form>
 </div>
