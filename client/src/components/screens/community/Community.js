bde718e2bee08e2756da7d61176fcaf86cff9cab
araz saydu <arazsaydu@gmail.com>
2022-03-03 10:17:01 +0100

    create post form with simple design and react functionality

--- client/src/components/screens/community/Community.js
+++ client/src/components/screens/community/Community.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import './community.css'
-//import Post from '../post/Post'
+import Post from '../post/Post'
 
 function Community() {
   return (
     <div className="">
-      <button className="postBtn" type="button"> post</button>
+      <Post/>
+      
     </div>
   )
 }
--- client/src/components/screens/post/Post.js
+++ client/src/components/screens/post/Post.js
@@ -1,51 +1,39 @@
-import React from 'react'
-import { Card, CardBody, CardImg, CardText, CardTitle } from 'reactstrap';
+import React, { useState } from 'react'
+import './post.css'
 
 function Post() {
+  const [title, setTitle] = useState('')
+  const [body, setBody] = useState('')
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    const poste = {title, body};
+    console.log(poste);
+  }
   return (
    <>
-  <Card>
-    <CardImg
-      alt="Card image cap"
-      src="https://picsum.photos/318/180"
-      top
-      width="100%"
-    />
-    <CardBody>
-      <CardTitle tag="h5">
-        Card Title
-      </CardTitle>
-      <CardText>
-        This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.
-      </CardText>
-      <CardText>
-        <small className="text-muted">
-          Last updated 3 mins ago
-        </small>
-      </CardText>
-    </CardBody>
-  </Card>
-  <Card>
-    <CardBody>
-      <CardTitle tag="h5">
-        Card Title
-      </CardTitle>
-      <CardText>
-        This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.
-      </CardText>
-      <CardText>
-        <small className="text-muted">
-          Last updated 3 mins ago
-        </small>
-      </CardText>
-    </CardBody>
-    <CardImg
-      alt="Card image cap"
-      bottom
-      src="https://picsum.photos/318/180"
-      width="100%"
-    />
-  </Card>
+    <form className="postForm" onSubmit={submitHandler}>
+    <h1 className="postH1">Share your expires</h1>
+      <label className="postLabel">Title</label>
+      <input
+       type="text"
+       required placeholder="title"
+       className="postInput"
+       value={title}
+       onChange={(e) => setTitle(e.target.value)}
+       ></input>
+      <label className="postLabel">Description</label>
+      <textarea
+       placeholder="description: in Berlin the weather is..."
+        required className="postText"
+        value ={body}
+        onChange = {(e) => setBody(e.target.value)}
+        ></textarea>
+
+      <img src="http://" className="postImg"/>
+
+      <button className="postBtn postButton"> post</button>
+    </form>
   </>
   )
 }
